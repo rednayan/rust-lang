d12d20cf2601ce47c6f351e7ef0e7d16b69f9ffa
nayan <nayansharma1010@gmail.com>
2023-02-20 12:39:04 +0530

    test3

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,12 +2,30 @@ struct Rect {
     width : u32,
     height : u32,
 }
-
 impl Rect {
     fn can_hold(&self , other : &Rect) -> bool {
         self.width > other.width && self.height > other.height
     }
 }
+
+pub struct Guess {
+    value : i32,
+}
+
+impl Guess {
+    fn new(value : i32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1");
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100"
+            );
+        }
+        Guess { value  }
+    }
+ }
+
+
 
 pub fn add(a:f32 , b:f32) -> f32 {
     a + b
@@ -48,6 +66,21 @@ mod tests {
         let b : f32 = 24.5;
 
         assert_eq!(36.7,add(a, b));
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn test_guess_value() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn it_works() -> Result<() , String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal to four"))
+        }
     }
     
 }
