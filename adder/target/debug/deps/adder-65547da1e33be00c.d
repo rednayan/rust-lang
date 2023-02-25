aacd6e524a0ec70aba202baa95dc7ddc5c087912
Nayan Sharma <51515645+rednayan@users.noreply.github.com>
2023-02-26 03:08:34 +0530

    Delete adder/target directory

--- adder/target/debug/deps/adder-65547da1e33be00c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\syien\Desktop\Desktop\rust-lang\adder\target\debug\deps\adder-65547da1e33be00c.exe: src\lib.rs
-
-C:\Users\syien\Desktop\Desktop\rust-lang\adder\target\debug\deps\adder-65547da1e33be00c.d: src\lib.rs
-
-src\lib.rs:
--- adder/target/debug/deps/integration_test-cce9c73c88782199.d
+++
@@ -1,5 +0,0 @@
-C:\Users\syien\Desktop\Desktop\rust-lang\adder\target\debug\deps\integration_test-cce9c73c88782199.exe: tests\integration_test.rs
-
-C:\Users\syien\Desktop\Desktop\rust-lang\adder\target\debug\deps\integration_test-cce9c73c88782199.d: tests\integration_test.rs
-
-tests\integration_test.rs:
