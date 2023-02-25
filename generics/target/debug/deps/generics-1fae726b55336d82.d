7c584e044ff1032127c1864680423949924598bc
Nayan Sharma <51515645+rednayan@users.noreply.github.com>
2023-02-26 03:11:26 +0530

    Delete generics/target directory

--- generics/target/debug/deps/generics-1fae726b55336d82.d
+++
@@ -1,5 +0,0 @@
-/Users/macbookair007/Documents/Rust/generics/target/debug/deps/generics-1fae726b55336d82.rmeta: src/main.rs
-
-/Users/macbookair007/Documents/Rust/generics/target/debug/deps/generics-1fae726b55336d82.d: src/main.rs
-
-src/main.rs:
--- generics/target/debug/deps/generics-299a0cd4954f179b.d
+++
@@ -1,5 +0,0 @@
-/Users/macbookair007/Documents/Rust/generics/target/debug/deps/generics-299a0cd4954f179b: src/main.rs
-
-/Users/macbookair007/Documents/Rust/generics/target/debug/deps/generics-299a0cd4954f179b.d: src/main.rs
-
-src/main.rs:
