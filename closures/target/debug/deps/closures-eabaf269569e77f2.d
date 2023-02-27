06fb78bb387c6825569b2fefacbc7a1f022d20c3
nayan <nayansharma1010@gmail.com>
2023-02-27 09:43:08 +0530

    closures

---
+++ closures/target/debug/deps/closures-eabaf269569e77f2.d
@@ -0,0 +1,5 @@
+c:\Users\syien\Desktop\Desktop\rust-lang\closures\target\debug\deps\closures-eabaf269569e77f2.rmeta: src\main.rs
+
+c:\Users\syien\Desktop\Desktop\rust-lang\closures\target\debug\deps\closures-eabaf269569e77f2.d: src\main.rs
+
+src\main.rs:
