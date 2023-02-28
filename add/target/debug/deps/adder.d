86b95b66a68fba651cd027330f69d84315c5aab9
Nayan Sharma <51515645+rednayan@users.noreply.github.com>
2023-02-28 11:32:43 +0530

    Delete add/target directory

--- add/target/debug/deps/adder.d
+++
@@ -1,5 +0,0 @@
-C:\Users\syien\Desktop\Desktop\rust-lang\add\target\debug\deps\adder.exe: adder\src\main.rs
-
-C:\Users\syien\Desktop\Desktop\rust-lang\add\target\debug\deps\adder.d: adder\src\main.rs
-
-adder\src\main.rs:
--- add/target/debug/libadd_one.d
+++
@@ -1,1 +0,0 @@
-C:\Users\syien\Desktop\Desktop\rust-lang\add\target\debug\libadd_one.rlib: C:\Users\syien\Desktop\Desktop\rust-lang\add\add_one\src\lib.rs
