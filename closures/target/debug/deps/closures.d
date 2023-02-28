d90a39393f79d7be7a3fd48c005710a61eb53d8f
Nayan Sharma <51515645+rednayan@users.noreply.github.com>
2023-02-28 11:33:30 +0530

    Delete closures/target directory

--- closures/target/debug/deps/closures.d
+++
@@ -1,5 +0,0 @@
-C:\Users\syien\Desktop\Desktop\rust-lang\closures\target\debug\deps\closures.exe: src\main.rs
-
-C:\Users\syien\Desktop\Desktop\rust-lang\closures\target\debug\deps\closures.d: src\main.rs
-
-src\main.rs:
