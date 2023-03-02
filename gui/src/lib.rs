f52bf971e94a7babf2acdcbda7db728ce6ca7e24
nayan <nayansharma1010@gmail.com>
2023-03-02 11:39:04 +0530

    Box<dyn>

---
+++ gui/src/lib.rs
@@ -0,0 +1,26 @@
+pub trait Draw {
+    fn draw(&self);
+}
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for components in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width : u32,
+    pub height : u32,
+    pub label; String,
+}
+
+impl Draw for Button {
+    for draw(&self){
+        // ------------//
+    }
+}
